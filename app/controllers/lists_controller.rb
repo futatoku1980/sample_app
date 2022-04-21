cc57977f4993fd8d98efdd61f8448153767e6fec
EC2 Default User <ec2-user@ip-172-31-85-151.ec2.internal>
2022-04-21 01:19:51 +0000

    [Finish]3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,11 +8,18 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
-  def edit
-  end
-
   def show
     @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+     list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
 
   # 以下を追加
